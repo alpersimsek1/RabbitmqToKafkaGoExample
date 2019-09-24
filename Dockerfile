94de5bac25c3af4ed8f018bc48bcb7a75b7f550f
Safa Keskin <keskinsaf@gmail.com>
2019-09-24 14:53:21 +0300

    feat(main.go): take queue configurations(url, port, name) as env. var refactor(main.go): wrap close calls into a function

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,15 @@
 FROM golang:alpine
 ADD . /go/src/app
 WORKDIR /go/src/app
+
+# ADJUST BELOW VARIABLES IN ORDER TO RUN PROCESS
+ARG RABBITMQ_URL_ARG=serverUrl
+ARG RABBITMQ_PORT_ARG=serverPort
+
+ENV RABBITMQ_URL=$RABBITMQ_URL_ARG
+ENV RABBITMQ_PORT=$RABBITMQ_PORT_ARG
+
 ENV PORT=3001
 RUN apk add git && go get github.com/segmentio/kafka-go && go get github.com/streadway/amqp
 CMD ["go", "run", "main.go"]
+
--- main.go
+++ main.go
@@ -2,64 +2,67 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"github.com/segmentio/kafka-go"
 	"github.com/streadway/amqp"
 	"log"
 	"os"
-	"time"
 )
 
 func failOnError(err error, msg string) {
 	if err != nil {
-		log.Fatalf("%s: %s", msg, err)
+		log.Printf("%s: %s", msg, err)
 	}
 }
 
-func Push(parent context.Context, key []byte, value []byte, w *kafka.Writer) (err error) {
-	message := kafka.Message{
-		Key:   key,
-		Value: value,
-		Time:  time.Now(),
-	}
-	return w.WriteMessages(parent, message)
+func closeOnFinish(conn *amqp.Connection, ch *amqp.Channel, kafkaConn *kafka.Conn) {
+	connErr := conn.Close()
+	failOnError(connErr, "Closing connection is failed!")
+	chErr := ch.Close()
+	failOnError(chErr, "Closing channel is failed!")
+	kafkaConnErr := kafkaConn.Close()
+	failOnError(kafkaConnErr, "Closing kafka connection is failed!")
 }
-
-var w *kafka.Writer
 
 func main() {
+	queueUrl := os.Getenv("RABBITMQ_URL")
+	queuePort := os.Getenv("RABBITMQ_PORT")
+	queueName := os.Getenv("QUEUE_NAME")
+	kafkaconn, _ := kafka.DialLeader(context.Background(), "udp", "kafka:9092", "test", 1)
 
-	kafkaconn, _ := kafka.DialLeader(context.Background(), "udp", "localhost:9092", "test", 5)
+	fmt.Println("queueUrl: " + queueUrl + ":" + queuePort)
+	fmt.Println("queueName: " + queueName)
+
 	w := kafka.NewWriter(kafka.WriterConfig{
-		Brokers: []string{"kafka:9092"},
-		Topic:   "test",
+		Brokers:  []string{"kafka:9092"},
+		Topic:    "test",
 		Balancer: &kafka.LeastBytes{},
 	})
-	conn, err := amqp.Dial("amqp://localhost:5672/")
+	conn, err := amqp.Dial("amqp://" + queueUrl + ":" + queuePort + "/")
 	failOnError(err, "Failed to connect to RabbitMQ")
-	defer conn.Close()
 
 	ch, err := conn.Channel()
 	failOnError(err, "Failed to open a channel")
-	defer ch.Close()
+	defer closeOnFinish(conn, ch, kafkaconn)
 
 	q, err := ch.QueueDeclare(
-		"processed_machine_action_notif", // name
-		true,                             // durable
-		false,                            // delete when unused
-		false,                            // exclusive
-		false,                            // no-wait
-		nil,                              // arguments
+		queueName, // name
+		true,      // durable
+		false,     // delete when unused
+		false,     // exclusive
+		false,     // no-wait
+		nil,       // arguments
 	)
 	failOnError(err, "Failed to declare a queue")
 
 	msgs, err := ch.Consume(
-		q.Name,          // queue
-		"test-consumer", // consumer
-		true,            // auto-ack
-		false,           // exclusive
-		false,           // no-local
-		false,           // no-wait
-		nil,             // args
+		q.Name,                // queue
+		queueName+"-consumer", // consumer
+		true,                  // auto-ack
+		false,                 // exclusive
+		false,                 // no-local
+		false,                 // no-wait
+		nil,                   // args
 	)
 	failOnError(err, "Failed to register a consumer")
 
@@ -69,14 +72,13 @@ func main() {
 		log.Printf("Consumer ready, PID: %d", os.Getpid())
 		for d := range msgs {
 			go func() {
-				log.Printf("Recei§ved a message: %s", d.Body)
-				w.WriteMessages(context.Background(),kafka.Message{Value: []byte(d.Body)})
+				log.Printf("Received a message: %s", d.Body)
+				err := w.WriteMessages(context.Background(), kafka.Message{Value: []byte(d.Body)})
+				failOnError(err, "Failed to write to kafka topic.")
 			}()
 		}
 	}()
 
 	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
 	<-forever
-
-	kafkaconn.Close()
 }
